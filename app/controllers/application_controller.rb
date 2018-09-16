a7142688fa8901d08fdcbe53436649ce5a0d57c6
inkanimal <theatomproject@gmail.com>
2018-09-16 15:26:28 -0400

    app controller commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,76 @@
+require './config/environment'
+require 'pry'
+
+class ApplicationController < Sinatra::Base
+
+   configure do
+    enable :sessions
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    set :session_secret, "password_security"
+   end
+
+
+   get '/' do
+    # if logged_in?
+    #   redirect to '/tweets'
+    # else
+      erb :index
+    # end
+   end
+
+   get '/signup' do
+     if logged_in?
+      redirect to '/tweets'
+    else
+      erb :'users/create_user'
+    end
+   end
+
+   post "/signup" do
+	    if params[:username].empty? || params[:password].empty? || params[:email].empty?
+       redirect  '/signup'
+     else
+	   user = User.new(username: params["username"], email: params["email"], password: params["password"])
+
+      if  user.save
+        session[:user_id] = user.id
+
+        redirect "/tweets"
+      end
+    end
+  end
+
+   get '/login' do
+    if logged_in?
+      redirect to '/tweets'
+    else
+      erb :'users/login'
+    end
+   end
+
+
+
+   post "/login" do
+     user = User.find_by(username: params[:username])
+
+     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+     else
+       redirect to '/login'
+     end
+   end
+
+   get "/logout" do
+    if logged_in?
+     session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+   end
+
+
+
+ end
